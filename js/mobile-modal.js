5610e930bb746d287e930c2b1e11b082dd0f55ae
Yaroslav Hoian <sgoyan1405@gmail.com>
2025-04-08 21:50:41 +0300

    markup mobile version

--- js/mobile-modal.js
+++ js/mobile-modal.js
@@ -1,17 +1,17 @@
 (() => {
    const refs = {
      // Додати атрибут data-modal-open на кнопку відкриття
-     openModalBtn: document.querySelector('[data-modal-open]'),
+     openModalBtn: document.querySelector('[data-mob-modal-open]'),
      // Додати атрибут data-modal-close на кнопку закриття
-     closeModalBtn: document.querySelector('[data-modal-close]'),
+     closeModalBtn: document.querySelector('[data-mob-modal-close]'),
      // Додати атрибут data-modal на бекдроп модалки
-     modal: document.querySelector('[data-modal]'),
+     modal: document.querySelector('[data-mob-modal]'),
    };
  
-   refs.openModalBtn.addEventListener('click', toggleModal);
-   refs.closeModalBtn.addEventListener('click', toggleModal);
+   refs.openModalBtn.addEventListener('click', toggleMobModal);
+   refs.closeModalBtn.addEventListener('click', toggleMobModal);
  
-   function toggleModal() {
+   function toggleMobModal() {
      // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
      refs.modal.classList.toggle('is-open');
    }
